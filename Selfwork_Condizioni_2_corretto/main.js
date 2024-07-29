f203b935e5c57fb708ebd4a623bd590401406ce7
LisviaIlvia <silviaercolino@yahoo.it>
2024-07-29 18:07:59 +0200

    Add files via upload

---
+++ Selfwork_Condizioni_2_corretto/main.js
@@ -0,0 +1,48 @@
+let temperatura = prompt('Quanti gradi ci sono oggi??');
+
+if(temperatura >= 0 && temperatura < 20){
+    console.log('non ci sono piu’ le mezze stagioni!');
+
+} else if(temperatura >= 30){
+    console.log('lu mare, lu sole, lu ientu...');
+
+} else if(temperatura >= 20 && temperatura < 30){
+    console.log('mi dia una peroni sudata');
+
+}else if(temperatura > -10 && temperatura < 0){
+    console.log('non e’ tanto il freddo quanto l’umidità');
+
+} else if(temperatura <= -10){
+    console.log('copriti…ancora ti raffreddi');
+} else {
+     console.log('temperatura non valida');
+}
+
+
+// let temperatura = prompt('Quanti gradi ci sono oggi??');
+
+// switch(true){
+
+//     case temperatura >= 0 && temperatura < 20:
+//         console.log('non ci sono piu’ le mezze stagioni!');
+//         break;
+
+//     case temperatura >= 30:
+//         console.log('lu mare, lu sole, lu ientu...');
+//         break;
+
+//     case temperatura >= 20 && temperatura < 30:
+//         console.log('mi dia una peroni sudata');
+//         break;
+
+//     case temperatura > -10 && temperatura < 0:
+//         console.log('non e’ tanto il freddo quanto l’umidità');
+//         break;
+
+//     case temperatura <= -10:
+//         console.log('copriti…ancora ti raffreddi');
+//         break;
+
+//     default:
+//         console.log('temperatura non valida');
+// }
